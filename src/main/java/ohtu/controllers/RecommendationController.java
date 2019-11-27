7aa05d5f0e96e6d5db2ad35d4f87760d7bdbc329
isokissa3 <koti94@windowslive.com>
2019-11-27 19:32:40 +0200

    Read recommendaitons

--- src/main/java/ohtu/controllers/RecommendationController.java
+++ src/main/java/ohtu/controllers/RecommendationController.java
@@ -2,10 +2,16 @@ package ohtu.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ohtu.database.entities.data.Book;
 import ohtu.database.entities.data.Link;
@@ -13,6 +19,8 @@ import ohtu.database.entities.data.Podcast;
 import ohtu.database.entities.recommendations.BookRecommendation;
 import ohtu.database.entities.recommendations.PodcastRecommendation;
 import ohtu.database.entities.recommendations.Recommendation;
+import ohtu.database.entities.recommendations.RecommendationType;
+import ohtu.database.entities.recommendations.StubRecommendation;
 import ohtu.database.entities.recommendations.VideoRecommendation;
 import ohtu.database.repositories.BookRepository;
 import ohtu.database.repositories.LinkRepository;
@@ -33,28 +41,91 @@ public class RecommendationController {
 	
     @Autowired
     private RecommendationRepository recommendationRepository;
-    
-    @GetMapping("/recommendations")
+
+    @GetMapping(value = {"/", "index"})
     public String list(Model model) {
-    	Link link = new Link("video", "lol");
-    	Book book = new Book("book", "lol", "lol");
+    	//TODO: Move all these three to AJAX calls
+    	List<Link> links = this.linkRepository.findAll();
+    	List<Book> books = this.bookRepository.findAll();
+    	List<Podcast> podcasts = this.podcastRepository.findAll();
     	
-    	Podcast podcast = new Podcast("podcast", "Waym", "Wew");
+    	List<Recommendation> recommendations = this.recommendationRepository.findAll();
+
+        model.addAttribute("links", links);
+        model.addAttribute("books", books);
+        model.addAttribute("podcasts", podcasts);
+        
+        model.addAttribute("recommendations", recommendations);
+    	model.addAttribute("recommendation", new StubRecommendation());
+        model.addAttribute("types", RecommendationType.valuesAsList());
     	
-    	this.linkRepository.save(link);
-    	this.bookRepository.save(book);
-    	this.podcastRepository.save(podcast);
-    	
-    	this.recommendationRepository.save(new VideoRecommendation(link));
-    	this.recommendationRepository.save(new BookRecommendation(book));
-    	this.recommendationRepository.save(new PodcastRecommendation(podcast));
-    	
-    	List<Recommendation> list = this.recommendationRepository.findAll();
-    	
-    	System.out.println(list);
-    	
-        model.addAttribute("recommendations", list);
-    	
-        return "recommendations";
+        return "index";
+    }
+
+    @PostMapping("index")
+    public String create(Model model, @ModelAttribute("recommendation") @Valid StubRecommendation stub, BindingResult result, RedirectAttributes redirectAttributes) {
+    	if (result.hasErrors()) {
+            return createError(model, stub);
+        }
+
+    	//TODO: Kinda ugly...
+    	Recommendation recommendation;
+    	if (stub.getType() == RecommendationType.BOOK) {
+    		Book book = this.bookRepository.findById(stub.getEntityId()).orElse(null);
+    		if (book == null) {
+    			return createErrorNotFound(model, stub);
+    		}
+    		
+    		recommendation = new BookRecommendation(book);
+    	} else if (stub.getType() == RecommendationType.VIDEO) {
+    		Link link = this.linkRepository.findById(stub.getEntityId()).orElse(null);
+    		if (link == null) {
+    			return createErrorNotFound(model, stub);
+    		}
+    		
+    		recommendation = new VideoRecommendation(link);
+    	} else if (stub.getType() == RecommendationType.PODCAST) {
+    		Podcast podcast = this.podcastRepository.findById(stub.getEntityId()).orElse(null);
+    		if (podcast == null) {
+    			return createErrorNotFound(model, stub);
+    		}
+    		
+    		recommendation = new PodcastRecommendation(podcast);
+    	} else {
+            return createErrorType(model, stub);
+    	}
+
+    	this.recommendationRepository.save(recommendation);
+
+        redirectAttributes.addFlashAttribute("message", "Lisäys onnistui!");
+
+        return "redirect:/index";
+    }
+    
+    private String createErrorNotFound(Model model, StubRecommendation stub) {
+		stub.setEntityId(0);
+		
+        return createError(model, stub);
+    }
+    
+    private String createErrorType(Model model, StubRecommendation stub) {
+		stub.setTypeId(null);
+		
+        return createError(model, stub);
+    }
+    
+    private String createError(Model model, StubRecommendation stub) {
+    	List<Link> links = this.linkRepository.findAll();
+    	List<Book> books = this.bookRepository.findAll();
+    	List<Podcast> podcasts = this.podcastRepository.findAll();
+
+        model.addAttribute("links", links);
+        model.addAttribute("books", books);
+        model.addAttribute("podcasts", podcasts);
+        
+    	model.addAttribute("recommendation", stub);
+        model.addAttribute("types", RecommendationType.valuesAsList());
+        
+        return "index";
     }
 }
---
+++ src/main/java/ohtu/database/entities/recommendations/RecommendationType.java
@@ -0,0 +1,39 @@
+package ohtu.database.entities.recommendations;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import lombok.Getter;
+
+public enum RecommendationType {
+	
+	BOOK("Kirja"),
+	VIDEO("Video"),
+	PODCAST("Podcast");
+	
+	private static final List<RecommendationType> VALUES = Collections.unmodifiableList(Arrays.asList(RecommendationType.values()));
+	
+	@Getter private final String name;
+	
+	private RecommendationType(String name) {
+		this.name = name;
+	}
+	
+	public String getTypeName() {
+		return super.name();
+	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	public static List<RecommendationType> valuesAsList() {
+		return RecommendationType.VALUES;
+	}
+	
+	public static RecommendationType valueOf(int ordinal) {
+		return RecommendationType.VALUES.get(ordinal);
+	}
+}
